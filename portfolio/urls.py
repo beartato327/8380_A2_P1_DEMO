0b99bb36a1040d113fe42bd56e9a705ec8f6dfda
beartato327 <rcapurro@unomaha.edu>
2021-02-03 09:26:58 -0600

    customer complete

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,16 @@
-from .views import CustomerList
 from django.urls import path
+from .views import (
+    CustomerList,
+    CustomerDelete,
+    CustomerUpdate,
+    CustomerCreate,
+    HomeView,
+    )
 
 urlpatterns = [
-    path('customer_list/', CustomerList.as_view(), name='customer_list'),
+    path('', HomeView.as_view(), name='home'),
+    path('customer_list', CustomerList.as_view(), name='customer_list'),
+    path('customer_add', CustomerCreate.as_view(), name='customer_add'),
+    path('customer/<int:pk>/edit', CustomerUpdate.as_view(), name='customer_update'),
+    path('customer/<int:pk>/delete', CustomerDelete.as_view(), name='customer_delete'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView, TemplateView
 from .models import Customer
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class CustomerList(ListView):
+class HomeView(TemplateView):
+    template_name = "portfolio/home.html"
+
+class CustomerList(LoginRequiredMixin,ListView):
     model = Customer
+
+class CustomerUpdate(LoginRequiredMixin, UpdateView):
+    model = Customer
+    fields = ['name', 'address', 'city', 'state', 'zipcode', 'email', 'cell_phone']
+
+class CustomerDelete(LoginRequiredMixin, DeleteView):
+    model = Customer
+    success_url = reverse_lazy('customer_list')
+
+class CustomerCreate(LoginRequiredMixin, CreateView):
+    model = Customer
+    fields = ['cust_number','name', 'address', 'city', 'state', 'zipcode', 'email', 'cell_phone']
+    success_url = reverse_lazy('customer_list')
